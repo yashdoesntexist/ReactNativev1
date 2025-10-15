c22411705d68e9c35d8a720b89fdbbc18760f108
agentP <learnhacksandtricks@gmail.com>
2025-10-14 21:35:09 -0700

    text, view, styles, input, buttons and useState

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
+import React, {useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Bruce');
+  const [age, setAge] = useState(25);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Enter your name:</Text>
+      <TextInput 
+      style={styles.input}
+      placeholder='Bruce Wayne'
+      onChangeText={(e)=> setName(e)}/>
+      <Text>Enter your age:</Text>
+      <TextInput 
+      keyboardType='number-pad'
+      style={styles.input}
+      placeholder='21'
+      onChangeText={(e)=> setAge(e)}/>
+        <Text style={styles.boldText}>Hello {name}, you are {age} years old</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +32,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  boldText:{
+    fontWeight:'bold',
+  },
+  buttonContainer:{
+    backgroundColor: "lightgrey",
+    marginTop: 20,
+  },
+  input:{
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+
+  },
+
 });
